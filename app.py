8672d36a7c707a75fc661ed1df09aea6d62cda95
Lovisa Johansson <lajjtis@gmail.com>
2015-01-08 10:47:55 +0800

    Update app.py
    added parameter for socket_timeout

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ logging.basicConfig()
 # Parse CLODUAMQP_URL (fallback to localhost)
 url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
 params = pika.URLParameters(url)
+params.socket_timeout = 5
 connection = pika.BlockingConnection(params) # Connect to CloudAMQP
 channel = connection.channel() # start a channel
 channel.queue_declare(queue='hello') # Declare a queue
